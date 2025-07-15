8331a361efcab64f349c3e04ec25104c2ca1f628
Nafis <nafisac45@gmail.com>
2025-07-16 02:17:57 +0600

    Colour Button

--- script/scripts.js
+++ script/scripts.js
@@ -1,3 +1,9 @@
+function changeBgColor(){
+    const randomColor = '#' + Math. floor(Math.random()*16777215). toString(16);
+    document.body.style.backgroundColor = randomColor;
+}
+
+
 let countIn = 23;
 let countDe = 6;
 let clickedButtons = new Set();
